e305b3accbc7e90211153a4641c7df1a86f5c837
WanDias06 <134244300+WanDias06@users.noreply.github.com>
2025-05-16 10:09:15 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SUA_AUTH_DOMAIN",
+  databaseURL: "SUA_DATABASE_URL",
+  projectId: "SUA_PROJECT_ID",
+  storageBucket: "SUA_STORAGE_BUCKET",
+  messagingSenderId: "SUA_SENDER_ID",
+  appId: "SUA_APP_ID"
+};
+firebase.initializeApp(firebaseConfig);
