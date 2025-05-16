2e0afc7413b89c68dcd37d3b0f05c5cb7ba9f51c
WanDias06 <134244300+WanDias06@users.noreply.github.com>
2025-05-16 11:41:31 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,18 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBwlMXu8fC253nMHStlzuJcwr7Uh-OWnEA",
-  authDomain: "plantae-app-d1111.firebaseapp.com",
-  databaseURL: "https://plantae-app-d1111-default-rtdb.firebaseio.com",
-  projectId: "plantae-app-d1111",
-  storageBucket: "plantae-app-d1111.firebasestorage.app",
-  messagingSenderId: "292265083240",
-  appId: "1:292265083240:web:32119a9bd4fcd5fb4e16dd",
-  measurementId: "G-47WNF13Y5P"
-};
-firebase.initializeApp(firebaseConfig);
+// SDK do Firebase versão 8.x (recomendado para projetos com JS puro)
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js"></script>
+
+<script>
+  const firebaseConfig = {
+    apiKey: "AIzaSyBwlMXu8fC253nMHStlzuJcwr7Uh-OWnEA",
+    authDomain: "plantae-app-d1111.firebaseapp.com",
+    databaseURL: "https://plantae-app-d1111-default-rtdb.firebaseio.com",
+    projectId: "plantae-app-d1111",
+    storageBucket: "plantae-app-d1111.firebasestorage.app",
+    messagingSenderId: "292265083240",
+    appId: "1:292265083240:web:32119a9bd4fcd5fb4e16dd"
+  };
+
+  // Inicializar o Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
